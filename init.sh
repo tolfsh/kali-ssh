c8954eecbdd974faafeda3e2737d1cf10638e06e
Théo Lorette-Froidevaux <github@tolf.sh>
2025-08-29 15:18:47 +0200

    add debug and password auth deactivation

--- init.sh
+++ init.sh
@@ -3,6 +3,12 @@
 # Définir le mot de passe de l'utilisateur kali si variable d'env présente
 if [ ! -z "$KALI_PASSWORD" ]; then
     echo "kali:$KALI_PASSWORD" | chpasswd
+    # Activer l'authentification par mot de passe
+    sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+else
+    # Désactiver l'authentification par mot de passe
+    sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+    sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 fi
 
 # Ajouter une clé publique si variable d'env présente
@@ -13,5 +19,9 @@ if [ ! -z "$KALI_PUBKEY" ]; then
     chmod 600 /home/kali/.ssh/authorized_keys
 fi
 
-# Démarrer le serveur SSH
-exec /usr/sbin/sshd -D -e
+# Démarrer le serveur SSH avec ou sans debug
+if [ ! -z "$DEBUG" ]; then
+    exec /usr/sbin/sshd -D -e
+else
+    exec /usr/sbin/sshd -D
+fi
